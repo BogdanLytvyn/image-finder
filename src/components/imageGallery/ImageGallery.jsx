4f728adff4d21dd2ff9becac8af86deeed33b8bb
Bogdan Lytvyn <72852244+BogdanLytvyn@users.noreply.github.com>
2021-03-01 19:58:41 +0200

    Done

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-export default function ImageGallery() {
+export default function ImageGallery({items}) {
     return (
-    <ul className="ImageGallery">
+        <ul className="ImageGallery">
+            {items.map(item => 
+                (<ImageGalleryIte />
+                    ))}
     </ul>
     )
 }
